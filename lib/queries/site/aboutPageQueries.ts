400876a216e1fba03b53310ba0d6c5227fa01ca6
devbiva <dev@bivatech.com>
2026-02-28 16:52:08 +0530

    feature: added about page

---
+++ lib/queries/site/aboutPageQueries.ts
@@ -0,0 +1,87 @@
+import { gql } from "@apollo/client";
+
+export const GET_ABOUT_PAGE = gql`
+    query AboutPageQuery {
+        pageBy(pageId: 40) {
+            pageOptions {
+                pageTitle
+                pageDescription
+            }
+            template {
+                ... on Template_AboutPage {
+                    templateName
+                    aboutPageBuilder {
+                        aboutPageBuilder {
+                            ... on AboutPageBuilderAboutPageBuilderHeroSectionLayout {
+                                fieldGroupName
+                                image {
+                                    node {
+                                        altText
+                                        sourceUrl
+                                    }
+                                }
+                                preTitle
+                                title
+                                description
+                                signatureTitle
+                                signatureSubtitle
+                            }
+                            ... on AboutPageBuilderAboutPageBuilderVisionMessageSectionLayout {
+                                fieldGroupName
+                                cards {
+                                    fieldGroupName
+                                    icon
+                                    title
+                                    description
+                                }
+                            }
+                            ... on AboutPageBuilderAboutPageBuilderCoreValuesSectionLayout {
+                                fieldGroupName
+                                sectionTitle
+                                values {
+                                    icon
+                                    title
+                                    description
+                                    fieldGroupName
+                                }
+                            }
+                            ... on AboutPageBuilderAboutPageBuilderBoardOfDirectorsSectionLayout {
+                                fieldGroupName
+                                sectionTitle
+                                sectionSubtitle
+                                directors {
+                                    image {
+                                        node {
+                                            altText
+                                            sourceUrl
+                                        }
+                                    }
+                                    name
+                                    role
+                                }
+                            }
+                            ... on AboutPageBuilderAboutPageBuilderClubHistorySectionLayout {
+                                fieldGroupName
+                                sectionTitle
+                                sectionSubtitle
+                                historyItems {
+                                    year
+                                    title
+                                    image {
+                                        node {
+                                            altText
+                                            sourceUrl
+                                        }
+                                    }
+                                    description
+                                    fieldGroupName
+                                }
+                                footerText
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
