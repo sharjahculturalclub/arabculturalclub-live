5c2f16429faaeea505e41a24557ea7ddabf63a27
devbiva <dev@bivatech.com>
2026-03-14 21:34:57 +0530

    fixed: resolved the forms acf issues

--- lib/queries/site/condolenceHallBookingPageQueries.ts
+++ lib/queries/site/condolenceHallBookingPageQueries.ts
@@ -5,53 +5,52 @@ import { gql } from "@apollo/client";
  */
 export const GET_CONDOLENCE_HALL_BOOKING_PAGE = gql`
     query condolenceHallBookingPageQuery {
-        pageBy(pageId: 480) {
-            pageOptions {
-                pageTitle
-                pageDescription
-            }
-            template {
-                ... on Template_CondolenceHallBookingPage {
-                    templateName
-                    condolenceHallBookingPageBuilder {
-                        lecturehallBookingPageBuilder {
-                            ... on CondolenceHallBookingPageBuilderLecturehallBookingPageBuilderCondolenceHallBookingFormSectionLayout {
-                                fieldGroupName
-                                formId
-                            }
-                            ... on CondolenceHallBookingPageBuilderLecturehallBookingPageBuilderInfoSectionLayout {
-                                fieldGroupName
-                                sectionTitle
-                                infoPoints {
-                                    pointText
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            sEOOptions {
-                seoTitle
-                metaDescription
-                focusKeyword
-                canonicalUrl
-                ogTitle
-                ogDescription
-                ogImage {
-                    node {
-                        altText
-                        sourceUrl
-                    }
-                }
-                twitterTitle
-                twitterDescription
-                twitterImage {
-                    node {
-                        altText
-                        sourceUrl
-                    }
-                }
-            }
-        }
+  pageBy(pageId: 480) {
+    pageOptions {
+      pageTitle
+      pageDescription
     }
+    template {
+      templateName
+      ... on Template_CondolenceHallBookingPage {
+        templateName
+        condolenceHallBookingPageBuilder {
+          condolenceHallBookingPageBuilder {
+            ... on CondolenceHallBookingPageBuilderCondolenceHallBookingPageBuilderCondolenceHallBookingFormSectionLayout {
+              formId
+            }
+            ... on CondolenceHallBookingPageBuilderCondolenceHallBookingPageBuilderInfoSectionLayout {
+              sectionTitle
+              infoPoints {
+                pointText
+              }
+            }
+          }
+        }
+      }
+    }
+    sEOOptions {
+      seoTitle
+      metaDescription
+      focusKeyword
+      canonicalUrl
+      ogTitle
+      ogDescription
+      ogImage {
+        node {
+          altText
+          sourceUrl
+        }
+      }
+      twitterTitle
+      twitterDescription
+      twitterImage {
+        node {
+          altText
+          sourceUrl
+        }
+      }
+    }
+  }
+}
 `;
--- lib/queries/site/swimmingSubscriptionPageQueries.ts
+++ lib/queries/site/swimmingSubscriptionPageQueries.ts
@@ -5,53 +5,52 @@ import { gql } from "@apollo/client";
  */
 export const GET_SWIMMING_SUBSCRIPTION_PAGE = gql`
     query SwimmingSubscriptionPageQuery {
-        pageBy(pageId: 444) {
-            pageOptions {
-                pageTitle
-                pageDescription
-            }
-            template {
-                ... on Template_SwimmingActivitySubscriptionPage {
-                    templateName
-                    swimmingSubscriptionPageBuilder {
-                        shareOpinionsPageBuilder {
-                            ... on SwimmingSubscriptionPageBuilderShareOpinionsPageBuilderSwimmingSubscriptionFormSectionLayout {
-                                fieldGroupName
-                                formId
-                            }
-                            ... on SwimmingSubscriptionPageBuilderShareOpinionsPageBuilderInfoSectionLayout {
-                                fieldGroupName
-                                sectionTitle
-                                infoPoints {
-                                    pointText
-                                }
-                            }
-                        }
-                    }
+    pageBy(pageId: 444) {
+        pageOptions {
+        pageTitle
+        pageDescription
+        }
+        template {
+        templateName
+        ... on Template_SwimmingActivitySubscriptionPage {
+            templateName
+            swimmingSubscriptionPageBuilder {
+            swimmingSubscriptionPageBuilder {
+                ... on SwimmingSubscriptionPageBuilderSwimmingSubscriptionPageBuilderSwimmingSubscriptionFormSectionLayout {
+                formId
+                }
+                ... on SwimmingSubscriptionPageBuilderSwimmingSubscriptionPageBuilderInfoSectionLayout {
+                sectionTitle
+                infoPoints {
+                    pointText
+                }
                 }
             }
-            sEOOptions {
-                seoTitle
-                metaDescription
-                focusKeyword
-                canonicalUrl
-                ogTitle
-                ogDescription
-                ogImage {
-                    node {
-                        altText
-                        sourceUrl
-                    }
-                }
-                twitterTitle
-                twitterDescription
-                twitterImage {
-                    node {
-                        altText
-                        sourceUrl
-                    }
-                }
             }
         }
+        }
+        sEOOptions {
+        seoTitle
+        metaDescription
+        focusKeyword
+        canonicalUrl
+        ogTitle
+        ogDescription
+        ogImage {
+            node {
+            altText
+            sourceUrl
+            }
+        }
+        twitterTitle
+        twitterDescription
+        twitterImage {
+            node {
+            altText
+            sourceUrl
+            }
+        }
+        }
+    }
     }
 `;
